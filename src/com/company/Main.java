e0e3ac03efb0c3adfc5787699913a86efcec7b42
TarasHrysiukk <okurva13_banan>
2020-10-21 22:44:02 +0300

    Вивід квадрату числа n

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 import static java.lang.Math.random;
 
 public class Main {
 
     public static void main(String[] args) {
-        int n = (int) (Math.random()*100);
+        int n = (int) (Math.random()*10000);
         System.out.println("Число n = " + n);
+        System.out.println("Число n^2 = " + n*n);
+
+
+
+
     }
 }
