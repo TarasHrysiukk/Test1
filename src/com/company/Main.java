fcb22a9dfa1b92a3933af9fe97cbb8f92cdb8b1f
TarasHrysiukk <okurva13_banan>
2020-10-21 20:30:10 +0300

    Перша спробаба коміту в локальному репозитоорію

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import static java.lang.Math.random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = (int) (Math.random()*100);
+        System.out.println("Число n = " + n);
+    }
+}
